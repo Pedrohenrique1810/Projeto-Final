c21fcf4e001747b092294e3200c9acc53f39b69f
Pedro <pedrohdstochetto1810@gmail.com>
2026-04-20 15:35:32 -0300

    remodelação do css e pequena alteração de nome no arquivo html

---
+++ color.js
@@ -0,0 +1,8 @@
+let layout = document.querySelector(".container");
+let options = document.querySelector(".container");
+let  = document.querySelector(".container");
+let container = document.querySelector(".container");
+let container = document.querySelector(".container");
+let container = document.querySelector(".container");
+let container = document.querySelector(".container");
+let container = document.querySelector(".container");
