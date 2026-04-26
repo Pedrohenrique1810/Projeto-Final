1bf56ef56307273cabb0af21c93f35206af03b83
Pedro <pedrohdstochetto1810@gmail.com>
2026-04-26 11:29:19 -0300

    Adição de variavel para uso em varios dispositivos movéis

--- color.js
+++ color.js
@@ -1,11 +1,13 @@
 let wrapper = document.querySelector(".wrapper");
-let gridButton = document.querySelector(".gridButtom");
-let cleargridButton = document.getElementById(".cleargridButton");
-let container = document.querySelector(".container");
-let container = document.querySelector(".container");
-let container = document.querySelector(".container");
-let container = document.querySelector(".container");
-let container = document.querySelector(".container");
+let gridButton = document.querySelector("submit-grid");
+let clearGridButton = document.getElementById("clear-grid");
+let gridWidth = document.querySelector("width-range");
+let gridHeigth = document.querySelector("heigth-range");
+let ColorButton = document.querySelector("color-btn");
+let erasebtn = document.querySelector("erase-btn");
+let paintBtn = document.querySelector("paint-btn");
+let widthValue = document.querySelector("width-value");
+let heigthValue = document.querySelector("heigth-value");
 
 let events {
     mouse: {
